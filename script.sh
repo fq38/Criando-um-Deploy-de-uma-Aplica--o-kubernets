9a0a656d5ae0e4ead92b89714172649ce77c96b8
fq38 <fq38.live.com>
2022-11-10 22:07:12 -0300

    v2

--- script.sh
+++ script.sh
@@ -8,8 +8,8 @@ docker build -t fq1444/projeto-database:1.0 database/.
 
 echo "realizando o push da imagens"
 
-docker push -t fq1444/projeto-backend:1.0
-docker push -t fq1444/projeto-database:1.0
+docker push  fq1444/projeto-backend:1.0
+docker push  fq1444/projeto-database:1.0
 
 echo "criando seviços no cluster kubernets.."
 
@@ -17,4 +17,4 @@ kubectl apply -f ./services.yml
 
 echo "criando os deployment"
 
-kubectl aplly -f ./deployment.yml
+kubectl apply -f ./deployment.yml
